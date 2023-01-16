d3f5a6fc20de440e30471637657f5a2c88466e50
Helen <helenmperham@gmail.com>
2023-01-16 15:46:08 -0500

    re-formatted menu_items in seeds

--- seeds.py
+++ seeds.py
@@ -12,31 +12,29 @@ with app.app_context():
 
 with app.app_context():
     menu_item1 = Menu_Item('Classic Ramen', '1','entree', 100, 8.00)
-    menu_item2 = Menu_Item(name: 'Negi-Goma Ramen', order_id: '1', category: 'entree', stock_remaining: 50, price: 12.00)
-    menu_item3 = Menu_Item(name: 'Miso Ramen', order_id: '1', category: 'entree', stock_remaining: 50, price: 10.00)
-    menu_item4 = Menu_Item(name: 'Shio Ramen', order_id: '1', category: 'entree', stock_remaining: 50, price: 10.00)
-    menu_item5 = Menu_Item(name: 'Curry Ramen', order_id: '1', category: 'entree', stock_remaining: 50, price: 12.00)
+    menu_item2 = Menu_Item('Negi-Goma Ramen', '1', 'entree', 50, 12.00)
+    menu_item3 = Menu_Item('Miso Ramen', '1', 'entree', 50, 10.00)
+    menu_item4 = Menu_Item('Shio Ramen', '1', 'entree', 50, 10.00)
+    menu_item5 = Menu_Item('Curry Ramen', '1', 'entree', 50, 12.00)
 
-    menu_item6 = Menu_Item(name: 'Pork Bun', order_id: '1', category: 'app', stock_remaining: 50, price: 4.50)
-    menu_item7 = Menu_Item(name: 'Shrimp Bun', order_id: '1', category: 'app', stock_remaining: 50, price: 4.50)
-    menu_item8 = Menu_Item(name: 'Gyoza', order_id: '1', category: 'app', stock_remaining: 100, price: 4.50)
+    menu_item6 = Menu_Item('Pork Bun', '1', 'app', 50, 4.50)
+    menu_item7 = Menu_Item('Shrimp Bun', '1', 'app', 50, 4.50)
+    menu_item8 = Menu_Item('Gyoza', '1', 'app', 100, 4.50)
 
 
-    menu_item9 = Menu_Item(name: 'Hot Tea', order_id: '1', category: 'bev', stock_remaining: 50, price: 3.00)
-    menu_item10 = Menu_Item(name: 'Soda', order_id: '1', category: 'bev', stock_remaining: 50, price: 3.00)
-    menu_item11 = Menu_Item(name: 'Seltzer', order_id: '1', category: 'bev', stock_remaining: 50, price: 3.00)
-    menu_item12 = Menu_Item(name: 'Sake Cup', order_id: '1', category: 'bev', stock_remaining: 50, price: 14.00)
-    menu_item13 = Menu_Item(name: 'Sake Bottle', order_id: '1', category: 'bev', stock_remaining: 50, price: 35.00)
+    menu_item9 = Menu_Item('Hot Tea', '1', 'bev', 50, 3.00)
+    menu_item10 = Menu_Item('Soda', '1', 'bev', 50, 3.00)
+    menu_item11 = Menu_Item('Seltzer', '1', 'bev', 50, 3.00)
+    menu_item12 = Menu_Item('Sake Cup', '1', 'bev', 50, 14.00)
+    menu_item13 = Menu_Item('Sake Bottle', '1', 'bev', 50, 35.00)
 
-    menu_item14 = Menu_Item(name: 'Green Tea Ice Cream', order_id: '1', category: 'dess', stock_remaining: 50, price: 3.00)
-    menu_item15 = Menu_Item(name: 'Red Bean Ice Cream', order_id: '1', category: 'dess', stock_remaining: 50, price: 3.00)
-    menu_item16 = Menu_Item(name: 'Mochi', order_id: '1', category: 'dess', stock_remaining: 50, price: 3.00)
-    menu_item17 = Menu_Item(name: 'Sesame Cookie', order_id: '1', category: 'dess', stock_remaining: 50, price: 3.00)
+    menu_item14 = Menu_Item('Green Tea Ice Cream', '1', 'dess', 50, 3.00)
+    menu_item15 = Menu_Item('Red Bean Ice Cream', '1', 'dess', 50, 3.00)
+    menu_item16 = Menu_Item('Mochi', '1', 'dess', 50, 3.00)
+    menu_item17 = Menu_Item('Sesame Cookie', '1', 'dess', 50, 3.00)
     db.session.add_all(
-        [menu_item1, menu_item2, menu_item3, menu_item4, 
-        menu_item5, menu_item6, menu_item7, 
-         menu_item8, menu_item9, menu_item10, 
-         menu_item11, menu_item12, menu_item13, menu_item14,
+        [menu_item1, menu_item2, menu_item3, menu_item4, menu_item5, menu_item6, menu_item7, 
+         menu_item8, menu_item9, menu_item10,menu_item11, menu_item12, menu_item13, menu_item14,
           menu_item15, menu_item16, menu_item17])
     db.session.commit()
 

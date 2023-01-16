f163a011fd771251d1ef716357eb0f96b9418123
Zak <zakedoubled@gmail.com>
2023-01-16 13:42:01 -0500

    get routes for managers/servers/staff

--- seeds.py
+++ seeds.py
@@ -1,7 +1,12 @@
 from app import app
-from models import db, User
+from models import db, Staff, Order, Reciept_Item
 
 print('Seeding database ... 🌱')
 # Add your seed data here
-
+with app.app_context():
+    server1 = Staff('Keenan', '12345')
+    server2 = Staff('Kel', '12345')
+    manager = Staff('Bob', '12345', True)
+    db.session.add_all([server1, server2, manager])
+    db.session.commit()
 print('Done! 🌳')
